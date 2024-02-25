c1cadaf343aa4154fe3903d1cba83e3cf15b46a2
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-02-25 12:05:26 +0900

    refactor

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,49 +1,168 @@
-# imports
-import os
 import sys
+import os
+import time
+import csv
 
+import click
 import torch
-from reinvent.runmodes.samplers.run_sampling import run_sampling
+import numpy as np
+from reinvent.config_parse import read_smiles_csv_file
+from reinvent.runmodes.dtos import ChemistryHelpers
+from reinvent.chemistry import Conversions
+from reinvent.chemistry.library_design import BondMaker, AttachmentPoints
+from reinvent.models.model_factory.sample_batch import SampleBatch
+from reinvent.runmodes.samplers.run_sampling import filter_valid
 
+from setup_sampler import setup_sampler
 
 # parse arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-# current file directory
-root = os.path.dirname(os.path.abspath(__file__))
+is_debug = False
+
+if __name__ == "__main__":
+    is_debug = True
+
+
+def sort_sampled_molecules(
+    sampled: SampleBatch, scores: "list[float]"
+) -> "tuple[SampleBatch, list[float]]":
+    """Sort sampled molecules using tanimoto in descending order."""
+    items1 = np.array(sampled.items1)
+    items2 = np.array(sampled.items2)
+    smilies = np.array(sampled.smilies)
+    states = np.array(sampled.states)
+    _scores = np.array(scores)
+
+    sorted_indices = np.argsort(_scores)[::-1]
+
+    sampled_batch = SampleBatch(
+        items1=list(items1[sorted_indices]),
+        items2=list(items2[sorted_indices]),
+        nlls=sampled.nlls[sorted_indices.copy()],
+        smilies=list(smilies[sorted_indices]),
+        states=list(states[sorted_indices]),
+    )
+
+    return sampled_batch, list(_scores[sorted_indices])
+
+
+def filter_out_duplicate_molecules(sampled: SampleBatch) -> SampleBatch:
+    """Filter out duplicate molecules from the sampled molecules.
+    It also remove the output molecules if it is similar to input molecules.
+
+    `sampled.items1` contains input smiles.
+    `sampled.smilies` contains output smiles.
+    """
+
+    seen = {}
+    items1 = []
+    items2 = []
+    states = []
+    smilies = []
+    nll_indices = []
+
+    for item1, item2, smile, nll_index, state in zip(
+        sampled.items1,
+        sampled.items2,
+        sampled.smilies,
+        range(len(sampled.items1)),
+        sampled.states,
+    ):
+        if smile in seen:
+            continue
+
+        seen[item1] = item1
+        seen[smile] = smile
+
+        items1.append(item1)
+        items2.append(item2)
+        smilies.append(smile)
+        states.append(state)
+        nll_indices.append(nll_index)
+
+    return SampleBatch(
+        items1=items1,
+        items2=items2,
+        states=states,
+        smilies=smilies,
+        nlls=sampled.nlls[nll_indices],
+    )
+
 
 # my model
 def my_model():
-     
-    # Path of the model.
-    MODEL = os.path.realpath(
-      os.path.join(
-        os.path.dirname(__file__),
-        '..',
-        '..',
-        'checkpoints',
-        'prior',
-        'mol2mol_similarity.prior'
+    batch_size = 2
+    chemistry = ChemistryHelpers(Conversions(), BondMaker(), AttachmentPoints())
+    sampler = setup_sampler(batch_size=batch_size, chemistry=chemistry)
+
+    input_smiles = None
+    num_input_smiles = 0
+
+    if os.path.exists(input_file):
+        input_smiles = read_smiles_csv_file(input_file, columns=0)
+        num_input_smiles = len(input_smiles)
+    else:
+        click.echo(click.style(f"[INPUT_FILE]: {input_file} doesn't exist.", fg="red"))
+        return
+
+    if not os.path.exists(os.path.dirname(os.path.abspath(output_file))):
+        click.echo(
+            click.style(
+                f"[OUTPUT_DIR]: {os.path.dirname(output_file)} doesn't exist.", fg="red"
+            )
         )
-      )
+        return
 
-    # Default config
-    config = {
-        "run_type": "sampling",
-        "parameters": {
-            "model_file": MODEL,
-            "unique_molecules": True,
-            "num_smiles": 100,
-            "output_file": output_file,
-            "smiles_file": input_file,
-            "sample_strategy": "beamsearch",
-            "temperature": 1.0
-        }
-    }
+    start_time = time.time()
 
-    run_sampling(config, torch.device('cpu'))
+    if is_debug:
+        click.echo(
+            click.style(
+                f"Starting sampling at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}",
+                fg="green",
+            )
+        )
+        click.echo(f"Total input smiles: {num_input_smiles}")
 
+    with torch.no_grad():
+        sampled = sampler.sample(input_smiles)
+    end_time = time.time()
+
+    if is_debug:
+        click.echo(
+            click.style(
+                f"Time taken in seconds: {int(end_time - start_time)}", fg="green"
+            )
+        )
+
+    # compute Tanimoto similarity between generated compounds and input compounds; return largest
+    _, valid_idxs = chemistry.conversions.smiles_to_mols_and_indices(sampled.items2)
+    valid_scores = sampler.calculate_tanimoto(input_smiles, sampled.items2)
+    scores = [None] * len(sampled.items2)
+    for i, j in enumerate(valid_idxs):
+        scores[j] = valid_scores[i]
+
+    sampled, scores = sort_sampled_molecules(sampled, scores)
+    sampled = filter_valid(sampled)
+    sampled = filter_out_duplicate_molecules(sampled)
+
+    total_smiles = len(sampled.smilies)
+
+    if is_debug:
+        expected_num_smiles = batch_size * num_input_smiles
+        click.echo(
+            click.style(
+                f"Total unique smiles generated: {total_smiles}, Expected: {expected_num_smiles}, Loss: {expected_num_smiles - total_smiles}"
+            )
+        )
+
+    HEADER = [f"Smiles {x}" for x in range(total_smiles)]
+
+    with open(output_file, "w", newline="") as fp:
+        csv_writer = csv.writer(fp)
+        csv_writer.writerows([HEADER, sampled.smilies, sampled.items1])
 
 
 # run model
