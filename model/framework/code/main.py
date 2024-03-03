1268f520be670d735f3ec78fe38d1c846d8487d6
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-03-03 16:11:42 +0900

    refactor: move utils func in main to utils.

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -6,94 +6,20 @@ import json
 
 import click
 import torch
-import numpy as np
 from reinvent.config_parse import read_smiles_csv_file
 from reinvent.runmodes.dtos import ChemistryHelpers
 from reinvent.chemistry import Conversions
 from reinvent.chemistry.library_design import BondMaker, AttachmentPoints
-from reinvent.models.model_factory.sample_batch import SampleBatch
 from reinvent.runmodes.samplers.run_sampling import filter_valid
 
 from setup_sampler import setup_sampler
+from utils import sort_sampled_molecules, filter_out_duplicate_molecules, pad_smiles
 
 # parse arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 is_debug = sys.argv[3] == "True" if len(sys.argv) > 3 else False
 log_file = output_file + ".json"
-
-
-def sort_sampled_molecules(
-    sampled: SampleBatch, scores: "list[float]"
-) -> "tuple[SampleBatch, list[float]]":
-    """Sort sampled molecules using tanimoto in descending order."""
-    items1 = np.array(sampled.items1)
-    items2 = np.array(sampled.items2)
-    smilies = np.array(sampled.smilies)
-    states = np.array(sampled.states)
-    _scores = np.array(scores)
-
-    sorted_indices = np.argsort(_scores)[::-1]
-
-    sampled_batch = SampleBatch(
-        items1=list(items1[sorted_indices]),
-        items2=list(items2[sorted_indices]),
-        nlls=sampled.nlls[sorted_indices.copy()],
-        smilies=list(smilies[sorted_indices]),
-        states=list(states[sorted_indices]),
-    )
-
-    return sampled_batch, list(_scores[sorted_indices])
-
-
-def filter_out_duplicate_molecules(sampled: SampleBatch) -> SampleBatch:
-    """Filter out duplicate molecules from the sampled molecules.
-    It also remove the output molecules if it is similar to input molecules.
-
-    `sampled.items1` contains input smiles.
-    `sampled.smilies` contains output smiles.
-    """
-
-    seen = {}
-    items1 = []
-    items2 = []
-    states = []
-    smilies = []
-    nll_indices = []
-
-    for item1, item2, smile, nll_index, state in zip(
-        sampled.items1,
-        sampled.items2,
-        sampled.smilies,
-        range(len(sampled.items1)),
-        sampled.states,
-    ):
-        seen[item1] = item1
-
-        if smile in seen:
-            if is_debug:
-                click.echo(
-                    click.style(
-                        f"Removing {smile}, as it is a duplicate entry.", fg="yellow"
-                    )
-                )
-            continue
-
-        seen[smile] = smile
-
-        items1.append(item1)
-        items2.append(item2)
-        smilies.append(smile)
-        states.append(state)
-        nll_indices.append(nll_index)
-
-    return SampleBatch(
-        items1=items1,
-        items2=items2,
-        states=states,
-        smilies=smilies,
-        nlls=sampled.nlls[nll_indices],
-    )
 
 
 # my model
@@ -151,7 +77,7 @@ def my_model():
 
     sampled, scores = sort_sampled_molecules(sampled, scores)
     sampled = filter_valid(sampled)
-    sampled = filter_out_duplicate_molecules(sampled)
+    sampled = filter_out_duplicate_molecules(sampled, is_debug=is_debug)
 
     total_smiles = len(sampled.smilies)
 
@@ -163,20 +89,22 @@ def my_model():
             )
         )
 
-    HEADER = [f"Smiles {x}" for x in range(total_smiles)]
+    output_smiles = pad_smiles(
+        sampled, input_smiles=input_smiles, target_length=batch_size
+    )
+    HEADER = [f"smi_{x}" for x in range(num_input_smiles * batch_size)]
 
     with open(output_file, "w", newline="") as fp:
         csv_writer = csv.writer(fp)
         # First Row: Header
         # Second Row: Generated Smiles (Output)
-        # Third Row: Input Smiles
-        csv_writer.writerows([HEADER, sampled.smilies, sampled.items1])
+        csv_writer.writerows([HEADER, output_smiles])
 
     with open(os.path.abspath(log_file), "w", newline="\n") as fp:
         log = {
             "start": start_time,
             "end": end_time,
-            "input_smiles": num_input_smiles,
+            "input_smiles": input_smiles,
             "total": total_smiles,
             "expected": expected_num_smiles,
         }
