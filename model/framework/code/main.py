094bf00686dac177ededfa0b0ae1404fe8b1df07
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-03-05 16:25:38 +0900

    refactor code

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,20 +1,13 @@
 # imports
 import sys
 import os
-import time
 import csv
 import json
 
 import click
-import torch
 from reinvent.config_parse import read_smiles_csv_file
-from reinvent.runmodes.dtos import ChemistryHelpers
-from reinvent.chemistry import Conversions
-from reinvent.chemistry.library_design import BondMaker, AttachmentPoints
-from reinvent.runmodes.samplers.run_sampling import filter_valid
 
-from setup_sampler import setup_sampler
-from utils import sort_sampled_molecules, filter_out_duplicate_molecules, pad_smiles
+from mol2mol_similarity_sampler import Mol2MolSimilaritySampler
 
 # parse arguments
 input_file = sys.argv[1]
@@ -29,96 +22,46 @@ is_debug = sys.argv[3] == "True" if len(sys.argv) > 3 else False
 log_file = output_file + ".json"
 
 
-# my model
-def my_model():
-    batch_size = 100
-    chemistry = ChemistryHelpers(Conversions(), BondMaker(), AttachmentPoints())
-    sampler = setup_sampler(batch_size=batch_size, chemistry=chemistry)
+batch_size = 100
+num_input_smiles = 0
+input_smiles = None
 
-    input_smiles = None
-    num_input_smiles = 0
 
-    if os.path.exists(input_file):
-        input_smiles = read_smiles_csv_file(input_file, columns=0)
-        num_input_smiles = len(input_smiles)
-    else:
-        click.echo(click.style(f"[INPUT_FILE]: {input_file} doesn't exist.", fg="red"))
-        return
+if os.path.exists(input_file):
+    input_smiles = read_smiles_csv_file(input_file, columns=0)
 
-    if not os.path.exists(os.path.dirname(os.path.abspath(output_file))):
-        click.echo(
-            click.style(
-                f"[OUTPUT_DIR]: {os.path.dirname(output_file)} doesn't exist.", fg="red"
-            )
+else:
+    click.echo(click.style(f"[INPUT_FILE]: {input_file} doesn't exist.", fg="red"))
+
+
+if not os.path.exists(os.path.dirname(os.path.abspath(output_file))):
+    click.echo(
+        click.style(
+            f"[OUTPUT_DIR]: {os.path.dirname(output_file)} doesn't exist.", fg="red"
         )
-        return
-
-    start_time = time.time()
-
-    if is_debug:
-        click.echo(
-            click.style(
-                f"Starting sampling at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}",
-                fg="green",
-            )
-        )
-        click.echo(f"Total input smiles: {num_input_smiles}")
-
-    with torch.no_grad():
-        sampled = sampler.sample(input_smiles)
-    end_time = time.time()
-
-    if is_debug:
-        click.echo(
-            click.style(
-                f"Time taken in seconds: {int(end_time - start_time)}", fg="green"
-            )
-        )
-
-    # compute Tanimoto similarity between generated compounds and input compounds; return largest
-    _, valid_idxs = chemistry.conversions.smiles_to_mols_and_indices(sampled.items2)
-    valid_scores = sampler.calculate_tanimoto(input_smiles, sampled.items2)
-    scores = [-1] * len(sampled.items2)
-    for i, j in enumerate(valid_idxs):
-        scores[j] = valid_scores[i]
-
-    sampled, scores = sort_sampled_molecules(sampled, scores)
-    sampled = filter_valid(sampled)
-    sampled = filter_out_duplicate_molecules(sampled, is_debug=is_debug)
-
-    total_smiles = len(sampled.smilies)
-
-    if is_debug:
-        expected_num_smiles = batch_size * num_input_smiles
-        click.echo(
-            click.style(
-                f"Total unique smiles generated: {total_smiles}, Expected: {expected_num_smiles}, Loss: {expected_num_smiles - total_smiles}"
-            )
-        )
-
-    output_smiles = pad_smiles(
-        sampled, input_smiles=input_smiles, target_length=batch_size
     )
-    HEADER = [f"smi_{x}" for x in range(num_input_smiles * batch_size)]
 
-    with open(output_file, "w", newline="") as fp:
-        csv_writer = csv.writer(fp)
-        # First Row: Header
-        # Second Row: Generated Smiles (Output)
-        csv_writer.writerows([HEADER, output_smiles])
+mol2mol_similarity = Mol2MolSimilaritySampler()
 
-    if is_debug:
-        with open(os.path.abspath(log_file), "w", newline="\n") as fp:
-            log = {
-                "start": start_time,
-                "end": end_time,
-                "input_smiles": input_smiles,
-                "total": total_smiles,
-                "expected": expected_num_smiles,
-            }
-
-            json.dump(log, fp)
+outputs, flatten_outputs, log = mol2mol_similarity.generate(
+    input_smiles=input_smiles, is_debug=is_debug
+)
 
 
-# run model
-my_model()
+input_len = len(input_smiles)
+output_len = len(outputs)
+assert input_len == output_len
+
+
+HEADER = [f"smi_{x}" for x in range(num_input_smiles * batch_size)]
+
+with open(output_file, "w", newline="") as fp:
+    csv_writer = csv.writer(fp)
+    # First Row: Header
+    # Second Row: Generated Smiles (Output)
+    csv_writer.writerows([HEADER, flatten_outputs])
+
+
+if is_debug:
+    with open(os.path.abspath(log_file), "w", newline="\n") as fp:
+        json.dump(log, fp)
---
+++ model/framework/code/mol2mol_similarity_sampler.py
@@ -0,0 +1,123 @@
+import os
+import time
+
+import click
+import torch
+from reinvent.runmodes import samplers, create_adapter
+from reinvent.runmodes.dtos import ChemistryHelpers
+from reinvent.chemistry import TransformationTokens, Conversions
+from reinvent.chemistry.library_design import BondMaker, AttachmentPoints
+from reinvent.runmodes.samplers.run_sampling import filter_valid
+
+
+from utils import (
+    sort_sampled_molecules,
+    filter_out_duplicate_molecules,
+    pad_smiles,
+    make_list_into_lists_of_n,
+)
+
+
+class Mol2MolSimilaritySampler:
+    ROOT = os.path.dirname(os.path.abspath(__file__))
+    CHECKPOINT = os.path.join(ROOT, "..", "..", "checkpoints")
+    MODEL = os.path.realpath(os.path.join(CHECKPOINT, "mol2mol_similarity.prior"))
+
+    def __init__(self):
+        self.batch_size = 100
+        self.chemistry = ChemistryHelpers(
+            Conversions(), BondMaker(), AttachmentPoints()
+        )
+
+        # Constants
+        temperature = 1.0
+        unique_sequences = True
+        isomeric = True
+        sample_strategy = "beamsearch"
+        tokens = TransformationTokens()
+        randomize_smiles = (
+            False  # For Mol2Mol reinvent 4 sets randomize_smiles to False.
+        )
+
+        # Creating adapter
+        agent, _, _ = create_adapter(self.MODEL, "inference", torch.device("cpu"))
+
+        # Creating sampler.
+        self.sampler = samplers.Mol2MolSampler(
+            agent,
+            batch_size=self.batch_size,
+            sample_strategy=sample_strategy,
+            isomeric=isomeric,
+            randomize_smiles=randomize_smiles,
+            unique_sequences=unique_sequences,
+            chemistry=self.chemistry,
+            temperature=temperature,
+            tokens=tokens,
+        )
+
+    def generate(
+        self, input_smiles: "list[str]", is_debug: bool
+    ) -> "tuple[list[list[str]], list[str], dict]":
+        start_time = time.time()
+        num_input_smiles = len(input_smiles)
+
+        if is_debug:
+            click.echo(
+                click.style(
+                    f"Starting sampling at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}",
+                    fg="green",
+                )
+            )
+            click.echo(f"Total input smiles: {num_input_smiles}")
+
+        with torch.no_grad():
+            sampled = self.sampler.sample(input_smiles)
+            end_time = time.time()
+
+            if is_debug:
+                click.echo(
+                    click.style(
+                        f"Time taken in seconds: {int(end_time - start_time)}",
+                        fg="green",
+                    )
+                )
+
+        # compute Tanimoto similarity between generated compounds and input compounds; return largest
+        _, valid_idxs = self.chemistry.conversions.smiles_to_mols_and_indices(
+            sampled.items2
+        )
+        valid_scores = self.sampler.calculate_tanimoto(input_smiles, sampled.items2)
+        scores = [-1] * len(sampled.items2)
+        for i, j in enumerate(valid_idxs):
+            scores[j] = valid_scores[i]
+
+        sampled, scores = sort_sampled_molecules(sampled, scores)
+
+        sampled = filter_valid(sampled)
+        sampled = filter_out_duplicate_molecules(sampled, is_debug=is_debug)
+
+        total_smiles = len(sampled.smilies)
+
+        if is_debug:
+            expected_num_smiles = self.batch_size * num_input_smiles
+            click.echo(
+                click.style(
+                    f"Total unique smiles generated: {total_smiles}, Expected: {expected_num_smiles}, Loss: {expected_num_smiles - total_smiles}"
+                )
+            )
+
+        flatten_outputs = pad_smiles(
+            sampled, input_smiles=input_smiles, target_length=self.batch_size
+        )
+
+        output_smiles = make_list_into_lists_of_n(flatten_outputs, num_input_smiles)
+
+        log = {
+            "start": start_time,
+            "end": end_time,
+            "input_smiles": input_smiles,
+            "total": total_smiles,
+            "expected": expected_num_smiles,
+        }
+
+        return (output_smiles, flatten_outputs, log)
