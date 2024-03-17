1db3ac3b36c188bbf7319dd6e44304b452ce0d0a
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-03-17 21:22:12 +0900

    pass batch size to the constructor

--- model/framework/code/mol2mol_similarity_sampler.py
+++ model/framework/code/mol2mol_similarity_sampler.py
@@ -23,8 +23,8 @@ class Mol2MolSimilaritySampler:
     CHECKPOINT = os.path.join(ROOT, "..", "..", "checkpoints")
     MODEL = os.path.realpath(os.path.join(CHECKPOINT, "mol2mol_similarity.prior"))
 
-    def __init__(self):
-        self.batch_size = 100
+    def __init__(self, bach_size):
+        self.batch_size = bach_size
         self.chemistry = ChemistryHelpers(
             Conversions(), BondMaker(), AttachmentPoints()
         )
